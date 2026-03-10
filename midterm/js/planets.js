b4ed1658093cb96b16bc38ac4613221bc8221a35
seansols1 <126801405+seansols1@users.noreply.github.com>
2026-03-10 11:57:29 -0500

    Add files via upload

---
+++ midterm/js/planets.js
@@ -0,0 +1,29 @@
+// API: 
+// https://api.visibleplanets.dev/v3?latitude=12&longitude=-981
+
+// Card Template:
+    // <div class="entry" id="">
+    // 	<figure>
+    //         <img src="" alt="" />
+    // 	<figcaption></figcaption>
+    // 	</figure>
+    // </div>
+
+$(document).ready(function() {
+    var bootstrapButton = $.fn.button.noConflict(); 
+	$.fn.bootstrapBtn = bootstrapButton;
+
+    // Write your code here
+
+     <div class="entry" id="">
+     	<figure>
+             <img src="../mercury.png" alt=" "></img>
+     	<figcaption></figcaption>
+     	</figure>
+     </div>
+     
+    
+    
+    
+    
+    })
